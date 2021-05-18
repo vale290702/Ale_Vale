80c3e9e7000a47ff249891033413ed9f993a0a5f
vale290702 <valecharpentier29@gmail.com>
2021-05-17 22:30:20 -0600

    encrypted code

---
+++ encrypted.py
@@ -0,0 +1,20 @@
+import time
+from os import system
+
+atbash = {'A' : 'Z', 'B' : 'Y', 'C' : 'X', 'D' : 'W', 'E' : 'V',
+        'F' : 'U', 'G' : 'T', 'H' : 'S', 'I' : 'R', 'J' : 'Q',
+        'K' : 'P', 'L' : 'O', 'M' : 'N', 'N' : 'M', 'O' : 'L',
+        'P' : 'K', 'Q' : 'J', 'R' : 'I', 'S' : 'H', 'T' : 'G',
+        'U' : 'F', 'V' : 'E', 'W' : 'D', 'X' : 'C', 'Y' : 'B', 'Z' : 'A', '.':'?', '?':'.'}
+
+def atbash_cypher(message):
+    finalMessage = ""
+    for letter in message.upper():
+        if letter != " ":
+            finalMessage += atbash[letter]
+        else:
+            finalMessage += " "
+
+    finalMessage = finalMessage.lower().capitalize()
+
+    return finalMessage
